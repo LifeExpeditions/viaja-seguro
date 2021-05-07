b5dc51ba080fc7dd6ae744694a8d2bc9360672f1
harold condori challco <yhon53190@gmail.com>
2021-05-06 20:46:03 -0500

    configuracion de base de datos

---
+++ api/db/index.js
@@ -0,0 +1,20 @@
+//# Dependencys
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+//# Connection to mongo DB
+export const connect = async () => {
+  try {
+    //const { FAZT_USER, FAZT_PASSWORD, FAZT_CLUSTER, FAZT_DB } = process.env;
+    await mongoose.connect(`mongodb://localhost:27017/taxisegure`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log(`🚀 The DB  is connected.`);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
